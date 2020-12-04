a6bb24060a890b382593343c9c815bb8c060641d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-12-04 16:12:08 -0300

    crashing on condition

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, Session, By, Condition } = require('selenium-webdriver');
 
 let driver = new Builder().forBrowser('firefox').build();
-let condition = new Promise( (resolve, reject) => {
-    if (resolve) {
-        setTimeout(resolve(true), 15000);
-    } else {
-        reject(false);
-    }
-});
 
-setTimeout(() => {condition = true}, 15000);
+let condition = new Condition('for auth').getCookies;
+
 driver.get('http://web.whatsapp.com');
-driver.wait(condition, 30);
+driver.wait(condition, 9000000);
+
+async function nameElement() {
+    let namesList = await driver.findElements(By.className("_1hI5g _1XH7x _1VzZY"));
+    let name = namesList.filter( name => {
+        if (name.title == "Luccas") {
+            return name;
+        }
+    });
+    console.log(namesList);
+    return name;
+}
+
