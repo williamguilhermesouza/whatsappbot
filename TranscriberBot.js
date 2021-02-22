42ce0a3d902ae1a065033df23161cb325a29c7f0
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-02-21 21:57:14 -0300

    trying to make bot transcribe audio

--- TranscriberBot.js
+++ TranscriberBot.js
@@ -1,5 +1,38 @@
 const { Builder, By, until, Key } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+async function speech_to_text() {
+
+    // Creates a client
+    const client = new speech.SpeechClient();
+
+    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+    const audio = {
+        content: fs.readFileSync('./input.ogg').toString('base64'),
+    };
+
+    const config = {
+        encoding: 'OGG_OPUS',
+        sampleRateHertz: 48000,
+        languageCode: 'pt-BR',
+    };
+    const request = {
+        audio: audio,
+        config: config,
+    };
+
+    // Detects speech in the audio file
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+    console.log(`Transcription: ${transcription}`);
+
+    return transcription;
+}
+
 
 let main = async () => {
     let options = new firefox.Options();
@@ -9,7 +42,6 @@ let main = async () => {
     options.setPreference("browser.helperApps.alwaysAsk.force", false);
     let driver = new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
     const actions = driver.actions({async: true});
-    const mouse = actions.mouse();
 
     await driver.get('http://web.whatsapp.com');
     await driver.wait(until.elementLocated(By.className("_1awRl copyable-text selectable-text")), 60000);
